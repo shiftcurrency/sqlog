501c529f0a4e4cff1a10d3222e38d2a81cf7a695
Shift Team <shiftcurrency@gmail.com>
2016-12-07 09:07:23 +0100

    added some functions for start, stop and status.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SQLOG
 SQLOG is a log parser and monitoring system that reads SHIFT/LISK log files. 
 The logs can then be worked with via a sql database doing all sorts of cool stuff.
+
+# Run
+nohup bash sqlog_collector.bash start -f /home/xxxx/shift/logs/shift.log &
+
+# Check status (running or not)
+bash sqlog_collector.bash status
+
+# Stop
+bash sqlog_collector.bash status
--- sqlog_collector.bash
+++ sqlog_collector.bash
@@ -1,32 +1,48 @@
 #!/usr/bin/env bash
-# Joey, <shiftcurrency@gmail.com>
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-logfile_to_parse=$1;
-
-if [[ -z "$logfile_to_parse" ]] || [[ ! -f "$logfile_to_parse" ]]; then
-    echo "Start with LISK/SHIFT log file as first argument. Specify full path.";
-    echo "Example: $0 /home/lisk/lisk-main/logs/lisk.log";
-fi
-
+running=0
+logfile_to_parse="lisk.log"
 sqlog_dir=$(dirname "$log_file_to_parse");
 sqlog_db="$sqlog_dir/sqlog.db";
 sqlog_logfile="$sqlog_dir/sqlog.log"
 sqlite=$(whereis sqlite3 |awk {'print $2'});
 
-if [[ -z "$sqlite" ]]; then
-    echo "SQLite3 not installed. Run: sudo apt-get install sqlite3";
-    exit 1;
-fi
+
+check_prereq() {
+
+    if [[ -z "$sqlite" ]]; then
+        echo "SQLite3 not installed. Run: sudo apt-get install sqlite3";
+        exit 1;
+    fi
+}
 
 create_db() {
 
     createdb='CREATE TABLE IF NOT EXISTS logs (datetime TIMESTAMP PRIMARY KEY NOT NULL, severity TEXT, log_string TEXT)';
     $sqlite $sqlog_db "$createdb" &> $sqlog_logfile || { echo "Could not create database. Check logfile $sqlog_logfile. Exiting." && exit 1; }
     echo "$(date): Created database $sqlog_db." >> $sqlog_logfile
+}
+
+running_status() {
+    
+    running=$(ps aux |grep 'sqlog_collector.bash start' |wc -l)
+    if [[ "$running" -gt "1" ]]; then
+        echo "OK";
+        exit 0;
+    else
+        echo "KO";
+        exit 1;
+    fi
+}
+
+stop_collector() {
+
+    ## It is safe to kill the process with "kill", it will do no harm to the SQLite database.
+    for i in $(ps aux |grep sqlog_collector.bash |awk {'print $2'}); do kill $i &> $sqlog_logfile; done
 }
 
 start_collector() {
@@ -42,8 +58,47 @@ start_collector() {
                 echo "$(date): Number of log lines parsed: $counter" >> $sqlog_logfile
             fi
         done
+    else
+        echo "$logfile_to_parse could not be found in this directory. Exiting."
+        exit 1
     fi
 }
 
-create_db
-start_collector
+
+parse_option() {
+    OPTIND=2
+    while getopts ":f:" opt; do
+    case $opt in
+    f)
+        if [[ -f "$OPTARG" ]] 2> /dev/null; then
+            logfile_to_parse=$OPTARG
+        fi
+    ;;
+    *) echo "Unknown option: -$OPTARG" >&2; exit 1
+    ;;
+    esac
+  done
+}
+
+parse_option $@
+
+case $1 in
+    "status")
+        running_status
+    ;;
+    "start")
+        check_prereq
+        create_db
+        start_collector
+    ;;
+    "stop")
+        stop_collector
+    ;;
+
+*)
+    echo 'Available options: start, stop, status'
+    echo 'Usage: ./sqlog_collector.bash start'
+    exit 1
+;;
+esac
+exit 0;
