40452e3b9a0c866fd207036c8019d01ef24cba00
Joey <shiftcurrency@gmail.com>
2016-12-08 19:33:20 +0000

    added return value explanations

--- sqlog.py
+++ sqlog.py
@@ -17,7 +17,7 @@ class LogParse(object):
         self.c = self.conn_db.cursor()
 
 
-    def check_nodes_height(self):
+    def consensus_height(self):
 
         heights = []
         ips = (config.get("general", "blockheight_nodes"))
@@ -35,16 +35,25 @@ class LogParse(object):
         if len(heights) > 0:
             heights.sort()
             return heights[-1]
+        return None
 
-        return False
+    def blockheight_low(self, consensus_height, own_height):
 
-    def blockheight_offset_rebuild(self, consensus_height, own_height):
-        if consensus_height and own_height and (int(own_height) <= int(consensus_height-20)) and not syncing():
-            if self.rebuild():
-                return True
+        """ Return values: 
+            True: Node is syncing blocks
+            False: Node is not syncing blocks """
+
+        if consensus_height and own_height and \
+            (int(own_height) <= int(consensus_height)-int(config.get("general", "block_offset"))):
+            return True
         return False
 
     def syncing(self):
+
+        """ Return values: 
+            height: as integer on success
+            None: if no result is returned
+            False: Exception """
 
         url = 'http://127.0.0.1:' + (config.get("general", "api_port")) + '/api/loader/status/sync'
         try:
@@ -54,10 +63,15 @@ class LogParse(object):
                 if res['syncing'] == True:
                     return True
         except Exception as e:
-            return True
+            return None
         return False
 
-    def check_own_height(self):
+    def own_blockheight(self):
+
+        """ Return values: 
+            height: as integer on success
+            None: if no result is returned
+            False: Exception """
 
         url = 'http://127.0.0.1:' + (config.get("general", "api_port")) + '/api/blocks/getHeight'
         try:
@@ -65,16 +79,24 @@ class LogParse(object):
             res = http_req.json()
             if 'success' in res and res['success']:
                 height = res['height']
+                return height
         except Exception as e:
             return False
-
-        return height
+        return None
 
     def start_collector(self):
 
         pid = subprocess.Popen([(config.get("paths", "sqlog_collector")), "start", "-f", (config.get("paths", "log_file"))]).pid
         if pid:
             return pid
+        return False
+
+    def restart(self):
+        p = subprocess.Popen("/bin/bash", (config.get("paths", "management_script")), "reload",  stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        proc_status = p.wait()
+        if proc_status == 0:
+            return True
         return False
 
     def rebuild(self):
@@ -87,9 +109,14 @@ class LogParse(object):
         return False
 
     def check_broadhash(self):
+
+        """ Return values:
+            False: broadhash consensus is bigger or equal to 51%
+            True: Broadhash is lower than 51%
+            None: Exception """
+
         try:
-            """ This SQL statement checks if broadhash consensus is lower than 100%. Further parsing will determine if its below 51% """
-            """ Return values. False: broadhash consensus is bigger or equal to 51%, True: broadhash is lower than 51% """
+             """ This SQL statement checks if broadhash consensus is lower than 100%. Further parsing will determine if its below 51% """
             sql = 'select * from logs where log_string like \'%Broadhash%\' and log_string not like \'%100%\'' + \
                     'AND (SELECT strftime(\'%Y-%m-%d %H:%M:%S\', datetime(\'now\', \'-20 seconds\'))) < datetime order by datetime DESC LIMIT 1'
 
@@ -104,9 +131,14 @@ class LogParse(object):
         return False
         
     def check_fork3(self):
+
+        """ Return values:
+        False: No fork type 3 detected within the specific time frame. 
+        True: There was a fork type 3 within the specific timeframe
+        None: Exception """
+
         try:
             """ This SQL statement checks if there was a fork of type 3 within now minus 20 seconds. """
-            """ Return values. False: no fork type 3 within the specific time frame, True: There was a fork type 3 within the specific timeframe """
             sql = 'SELECT * FROM logs WHERE log_string LIKE \'%Fork%\' AND log_string LIKE \'%"cause":3%\' AND (SELECT strftime(\'%Y-%m-%d %H:%M:%S\',' + \
                     'datetime(\'now\', \'-20 seconds\'))) < datetime'
 
